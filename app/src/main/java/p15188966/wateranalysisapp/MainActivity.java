d111819d83e51619b9803c2c251f54a8b717926b
Safrapath <P15188966@my365.dmu.ac.uk>
2018-01-31 15:36:27 +0000

    Started actuall making a camera app

--- app/src/main/java/p15188966/wateranalysisapp/MainActivity.java
+++ app/src/main/java/p15188966/wateranalysisapp/MainActivity.java
@@ -1,46 +1,58 @@
+//https://inducesmile.com/android/android-camera2-api-example-tutorial/
+
 package p15188966.wateranalysisapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Paint;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CaptureRequest;
+import android.media.ImageReader;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Size;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.TextureView;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "AndroidCameraApi";
+    private Button takePictureButton;
+    private TextureView textureView;
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90,90);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+    private String cameraId;
+    protected CameraDevice cameraDevice;
+    protected CameraCaptureSession cameraCaptureSessions;
+    protected CaptureRequest captureRequest;
+    protected CaptureRequest.Builder captureRequestBuilder;
+    private Size imageDimension;
+    private ImageReader imageReader;
+    private File file;
+    private static final int REQUEST_CAMERA_PERMISSION = 200;
+    private boolean mFlashSupported;
+    private Handler mBackgroundHandler;
+    private HandlerThread mBackgroundThread;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
-
-
-
-
-
-
 
     }
+
+
 }
-
-
-
-
-
-//    Button addBtn = findViewById(R.id.addBtn);
-//        addBtn.setOnClickListener(new View.OnClickListener() {
-//          @Override
-//          public void onClick(View view) {
-//        EditText firstNumEditText = findViewById(R.id.firstNumEditText);
-//        EditText secondNumEditText = findViewById(R.id.secondNumEditText);
-//        TextView resultTextView = findViewById(R.id.resultTextView);
-//
-//        int num1 = Integer.parseInt(firstNumEditText.getText().toString());
-//        int num2 = Integer.parseInt(secondNumEditText.getText().toString());
-//        int result = num1 + num2;
-//
-//        resultTextView.setText(result + "");
-//        }
-//        });
