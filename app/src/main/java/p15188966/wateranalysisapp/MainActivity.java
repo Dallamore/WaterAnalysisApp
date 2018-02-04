c4bb06c3a74b8a895ef1080053adca0590693777
Safrapath <P15188966@my365.dmu.ac.uk>
2018-02-04 23:04:23 +0000

    Fixed image rotating in ImageView on main activity

--- app/src/main/java/p15188966/wateranalysisapp/MainActivity.java
+++ app/src/main/java/p15188966/wateranalysisapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -25,170 +27,203 @@ import java.util.Locale;
 
 public class MainActivity extends Activity {
 
-        private static final String BITMAP_STORAGE_KEY = "viewbitmap";
-        private static final String IMAGEVIEW_VISIBILITY_STORAGE_KEY = "imageviewvisibility";
-        private ImageView mImageView;
-        private Bitmap mImageBitmap;
-        private String mCurrentPhotoPath;
+    private static final String BITMAP_STORAGE_KEY = "viewbitmap";
+    private static final String IMAGEVIEW_VISIBILITY_STORAGE_KEY = "imageviewvisibility";
+    private ImageView mImageView;
+    private Bitmap mImageBitmap;
+    private String mCurrentPhotoPath;
 
-        private File createImageFile() throws IOException {
-            // Create an image file name
-            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(new Date());
-            String imageFileName = "JPEG_" + timeStamp + "_";
-            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-            File image = File.createTempFile(
-                    imageFileName,
-                    ".jpg",
-                    storageDir
-            );
-            mCurrentPhotoPath = image.getAbsolutePath();
-            return image;
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        System.out.println("boobies "+storageDir.toString());
+        File image = File.createTempFile(imageFileName,".jpg",storageDir);
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private Bitmap rotateImage(Bitmap bitmap){
+        ExifInterface exifInterface = null;
+        try{
+            exifInterface = new ExifInterface(mCurrentPhotoPath);
+        } catch (IOException e){
+            e.printStackTrace();
         }
+        int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+        Matrix matrix = new Matrix();
+        switch (orientation){
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                matrix.setRotate(90);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                matrix.setRotate(180);
+                break;
+            default:
+                break;
+        }
+        Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
 
-        private void setPic() {
+        return rotatedBitmap;
+    }
+
+    private void setPic() {
 
 		/* There isn't enough memory to open up more than a couple camera photos */
 		/* So pre-scale the target bitmap into which the file is decoded */
 
 		/* Get the size of the ImageView */
-            int targetW = mImageView.getWidth();
-            int targetH = mImageView.getHeight();
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
 
 		/* Get the size of the image */
-            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-            bmOptions.inJustDecodeBounds = true;
-            BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-            int photoW = bmOptions.outWidth;
-            int photoH = bmOptions.outHeight;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
 
 		/* Figure out which way needs to be reduced less */
-            int scaleFactor = 1;
-            if ((targetW > 0) || (targetH > 0)) {
-                scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-            }
+        int scaleFactor = 1;
+        if ((targetW > 0) || (targetH > 0)) {
+            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        }
 
 		/* Set bitmap options to scale the image decode target */
-            bmOptions.inJustDecodeBounds = false;
-            bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
 
 		/* Decode the JPEG file into a Bitmap */
-            Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
 
 		/* Associate the Bitmap to the ImageView */
-            mImageView.setImageBitmap(bitmap);
-            mImageView.setVisibility(View.VISIBLE);
-        }
 
-        private void galleryAddPic() {
-            Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
-            File f = new File(mCurrentPhotoPath);
-            Uri contentUri = Uri.fromFile(f);
-            mediaScanIntent.setData(contentUri);
-            this.sendBroadcast(mediaScanIntent);
-        }
+        mImageView.setImageBitmap(rotateImage(bitmap));
+        mImageView.setVisibility(View.VISIBLE);
+    }
 
-        private void dispatchTakePictureIntent() {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                //Create file where the photo should go
-                File photoFile;
-                try {
-                    photoFile = createImageFile();
-                }catch (IOException ex){
-                    //Error occurred
-                    ex.printStackTrace();
-                    photoFile = null;
-                    mCurrentPhotoPath = null;
-                }
-                //Continue only if the file was successfully created
-                if (photoFile != null) {
-                    Uri photoUri = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-                    startActivityForResult(takePictureIntent, 1);
-                }
-            }
-        }
+//        private void galleryAddPic() {
+//            Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+//            File f = new File(mCurrentPhotoPath);
+//            Uri contentUri = Uri.fromFile(f);
+//            mediaScanIntent.setData(contentUri);
+//            this.sendBroadcast(mediaScanIntent);
+//        }
 
-        private void handleBigCameraPhoto() {
-            if (mCurrentPhotoPath != null) {
-                setPic();
-                galleryAddPic();
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            //Create file where the photo should go
+            File photoFile;
+            try {
+                photoFile = createImageFile();
+            }catch (IOException ex){
+                //Error occurred
+                ex.printStackTrace();
+                photoFile = null;
                 mCurrentPhotoPath = null;
             }
-
-        }
-
-        Button.OnClickListener mTakePicOnClickListenerB =
-                new Button.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dispatchTakePictureIntent();
-                    }
-                };
-
-        /** Called when the activity is first created. */
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            mImageView = findViewById(R.id.imageView1);
-            mImageBitmap = null;
-
-            Button picBtnB = findViewById(R.id.btnCapture);
-            setBtnListenerOrDisable(
-                    picBtnB,
-                    mTakePicOnClickListenerB,
-                    MediaStore.ACTION_IMAGE_CAPTURE
-            );
-        }
-
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            if (resultCode == RESULT_OK) {
-                handleBigCameraPhoto();
+            //Continue only if the file was successfully created
+            if (photoFile != null) {
+                Uri photoUri = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, 1);
             }
         }
+    }
 
-        // Some lifecycle callbacks so that the image can survive orientation change
-        @Override
-        protected void onSaveInstanceState(Bundle outState) {
-            outState.putParcelable(BITMAP_STORAGE_KEY, mImageBitmap);
-            outState.putBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY, (mImageBitmap != null) );
-            super.onSaveInstanceState(outState);
+    private void handleBigCameraPhoto() {
+        if (mCurrentPhotoPath != null) {
+            setPic();
+            galleryAddPic();
+            mCurrentPhotoPath = null;
         }
 
-        @Override
-        protected void onRestoreInstanceState(Bundle savedInstanceState) {
-            super.onRestoreInstanceState(savedInstanceState);
-            mImageBitmap = savedInstanceState.getParcelable(BITMAP_STORAGE_KEY);
-            mImageView.setImageBitmap(mImageBitmap);
-            mImageView.setVisibility(
-                    savedInstanceState.getBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY) ?
-                            ImageView.VISIBLE : ImageView.INVISIBLE
-            );
-        }
+    }
 
-        public static boolean isIntentAvailable(Context context, String action) {
-            final PackageManager packageManager = context.getPackageManager();
-            final Intent intent = new Intent(action);
-            List<ResolveInfo> list =
-                    packageManager.queryIntentActivities(intent,
-                            PackageManager.MATCH_DEFAULT_ONLY);
-            return list.size() > 0;
-        }
+    Button.OnClickListener mTakePicOnClickListenerB =
+            new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dispatchTakePictureIntent();
+                }
+            };
 
-        private void setBtnListenerOrDisable(
-                Button btn,
-                Button.OnClickListener onClickListener,
-                String intentName
-        ) {
-            if (isIntentAvailable(this, intentName)) {
-                btn.setOnClickListener(onClickListener);
-            } else {
-                btn.setText(
-                        getText(R.string.cannot).toString());
-                btn.setClickable(false);
-            }
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.capturePhotoImageView);
+        mImageBitmap = null;
+
+        Button picBtnB = findViewById(R.id.btnCapture);
+        setBtnListenerOrDisable(
+                picBtnB,
+                mTakePicOnClickListenerB,
+                MediaStore.ACTION_IMAGE_CAPTURE
+        );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            handleBigCameraPhoto();
         }
+    }
+
+    // Some lifecycle callbacks so that the image can survive orientation change
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelable(BITMAP_STORAGE_KEY, mImageBitmap);
+        outState.putBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY, (mImageBitmap != null) );
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mImageBitmap = savedInstanceState.getParcelable(BITMAP_STORAGE_KEY);
+        mImageView.setImageBitmap(mImageBitmap);
+        mImageView.setVisibility(
+                savedInstanceState.getBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY) ?
+                        ImageView.VISIBLE : ImageView.INVISIBLE
+        );
+    }
+
+    public static boolean isIntentAvailable(Context context, String action) {
+        final PackageManager packageManager = context.getPackageManager();
+        final Intent intent = new Intent(action);
+        List<ResolveInfo> list =
+                packageManager.queryIntentActivities(intent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+
+    private void setBtnListenerOrDisable(
+            Button btn,
+            Button.OnClickListener onClickListener,
+            String intentName
+    ) {
+        if (isIntentAvailable(this, intentName)) {
+            btn.setOnClickListener(onClickListener);
+        } else {
+            btn.setText(
+                    getText(R.string.cannot).toString());
+            btn.setClickable(false);
+        }
+    }
 }
